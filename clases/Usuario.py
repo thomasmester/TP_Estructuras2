f42b7be3f8a484190d7b3014eb2148b0e8346291
thomasmester <mester2@gmail.com>
2023-05-22 18:53:42 -0300

    json case1 v2

--- clases/Usuario.py
+++ clases/Usuario.py
@@ -2,5 +2,5 @@ class Usuario:
     def __init__(self, nombre, apellido, DNI, email):
         self.nombre = nombre
         self.apellido = apellido
-        self.sexo = DNI
-        self.edad = email
+        self.DNI = DNI
+        self.email = email
