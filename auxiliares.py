7a7009163ca999af2d261130f1a607c598765b37
thomasmester <mester2@gmail.com>
2023-06-01 19:50:02 -0300

    guardar club jsonizado 50%

--- auxiliares.py
+++ auxiliares.py
@@ -16,7 +16,24 @@ def jsonHandler(archivo):
         except json.decoder.JSONDecodeError:
             jsonData = []
         return jsonData
+    
+def clubADicts(club):
+    cDict = club.__dict__
+    for i in range(len(cDict['lista_socios'])):
+        cDict['lista_socios'][i] = cDict['lista_socios'][i].__dict__
+    for i in range(len(cDict['lista_pagos'])):
+        cDict['lista_pagos'][i] = cDict['lista_pagos'][i].__dict__
+        cDict['lista_pagos'][i]['fecha'] = str(cDict['lista_pagos'][i]['fecha'])
+    for i in range(len(cDict['lista_empleados'])):
+        cDict['lista_empleados'][i] = cDict['lista_empleados'][i].__dict__
+    for i in range(len(cDict['lista_instalaciones'])):
+        cDict['lista_instalaciones'][i] = cDict['lista_instalaciones'][i].__dict__
+        for j in range(len(cDict['lista_instalaciones'][i]['lista_reservas'])):
+            cDict['lista_instalaciones'][i]['lista_reservas'][j] = cDict['lista_instalaciones'][i]['lista_reservas'][j].__dict__
+            cDict['lista_instalaciones'][i]['lista_reservas'][j]['fechaReserva'] = str(cDict['lista_instalaciones'][i]['lista_reservas'][j]['fechaReserva'])
+    return cDict
 
+    return cDict
 def splitearLista(lista, var):
     # recibe una lista de strings a splitear
     for i in range(len(lista)):
